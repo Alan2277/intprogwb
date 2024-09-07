1033336fdf3b9a0731a36f5c916bd22dae0127be
Alan Sampaio <universitario.alan777@gmail.com>
2024-09-07 01:19:39 -0300

    realizado mais da metade

--- 022/js/script.js
+++ 022/js/script.js
@@ -1,5 +1,50 @@
-var num = parseInt(prompt('Informe um número:'));
+function informarAlert() {
+    alert('Teste JS')
+}
 
-for (let i = 1; i <= num; i++) {
-    console.log(i);
+function minhaFuncao(arg1, arg2) {
+    var soma = arg1 + arg2;
+
+    alert(`O argumento 01 = ${arg1} somado com o argumento 02 = ${arg2} eh igual a ${soma} `);
+
+    var flag = true;
+
+    if (flag)
+        alert('Verdadeiro')
+    else
+        alert('Falso')
+}
+
+function raizQuadrada(arg) {
+    var raiz = Math.sqrt(arg);
+
+    alert(`A Raiz Quadrada de ${arg} é ${raiz}`);
+
+}
+
+function escrever(infor) {
+
+    document.write(infor)
+
+}
+
+function escreverPrompt() {
+
+    var pegar_informacao = prompt("Informe algum texto:")
+
+    document.getElementById("minhaDiv").innerHTML = pegar_informacao;
+}
+
+function cont_numeros() {
+
+    var numero = parseInt(prompt("Informe um número:"));
+
+    var contador = '';
+
+    for (var i = 1; i <= numero; i++) {
+        contador += i + "<br>";
+    }
+
+    document.getElementById("numeros").innerHTML = contador;
+
 }
